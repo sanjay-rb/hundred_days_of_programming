270bda0fbb092ee0109c456642c1f329c63abc78
sanjay-rb <sanjaybabu618@gmail.com>
2024-06-22 17:57:03 +0530

    Home Page UI
    - Generated 100 Days Data
    - Updated data
    - Added firebase config
    - Added firebase crashlytics & analytics
    - Updated readme and license
    - Added icon and deleted unwanted files
    - Updated gitignore & Removed sdk.properties

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,14 +1,110 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Text("Home"),
+        child: ListView(
+          children: [
+            const SizedBox(height: 10),
+            const Center(
+              child: Text(
+                "( Drag down to refresh )",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 8,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Total Task Completed",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Text(
+                      "10/100",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            FutureBuilder<Map>(
+                future: readJson(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container();
+                  }
+                  Map? data = snapshot.data;
+                  return Column(
+                    children: List.generate(
+                      100,
+                      (index) => Dismissible(
+                        background: Container(
+                          color: Colors.blue,
+                          padding: const EdgeInsets.all(10),
+                          alignment: Alignment.centerRight,
+                          child: const Icon(
+                            Icons.navigate_next,
+                            color: Colors.white,
+                          ),
+                        ),
+                        direction: DismissDirection.endToStart,
+                        confirmDismiss: (direction) async {
+                          if (direction == DismissDirection.endToStart) {
+                            return false; // Prevent dismissing for the update action
+                          }
+                          return true;
+                        },
+                        key: ValueKey(index + 1),
+                        child: ListTile(
+                          trailing: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "🔴", // 🟢
+                              ),
+                            ],
+                          ),
+                          title: Text('Day ${data!["${index + 1}"]['id']}'),
+                          subtitle: Text(data["${index + 1}"]['title']),
+                          isThreeLine: true,
+                          leading: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.lock_outline),
+                            ],
+                          ),
+                          onTap: () {},
+                        ),
+                      ),
+                    ).toList(),
+                  );
+                }),
+          ],
+        ),
       ),
     );
   }
+}
+
+Future<Map> readJson() async {
+  final String response = await rootBundle.loadString('assets/data/input.json');
+  Map data = await json.decode(response);
+  return data;
 }
