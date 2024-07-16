67ca988ab15ef27f6bb1a4c83d0159f5c46f4c09
sanjay-rb <sanjaybabu618@gmail.com>
2024-07-16 10:26:32 +0530

    render box error fix

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -29,113 +29,134 @@ class LoginView extends GetView<LoginController> {
                 child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Center(
-                      child: Card(
-                        color: Get.theme.colorScheme.primary,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Form(
-                            key: controller.formKey,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                ClipOval(
-                                  child: SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width * .15,
-                                    child: Image.asset('assets/icon/icon.png'),
-                                  ),
-                                ),
-                                Obx(
-                                  () => controller.isLogin.value
-                                      ? const SizedBox()
-                                      : const SizedBox(height: 25),
-                                ),
-                                Obx(
-                                  () => controller.isLogin.value
-                                      ? const SizedBox()
-                                      : UiTextFormFieldWidget(
-                                          controller: controller.name,
-                                          validator: (value) {
-                                            return controller
-                                                .validateFormFields(value);
-                                          },
-                                          label: "Name",
-                                          bgColor:
-                                              Get.theme.colorScheme.secondary,
-                                          textColor:
-                                              Get.theme.colorScheme.tertiary,
-                                          icon: Icons.account_circle,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Card(
+                              color: Get.theme.colorScheme.primary,
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Form(
+                                  key: controller.formKey,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      ClipOval(
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              .15,
+                                          child: Image.asset(
+                                              'assets/icon/icon.png'),
                                         ),
-                                ),
-                                const SizedBox(height: 25),
-                                UiTextFormFieldWidget(
-                                  controller: controller.email,
-                                  validator: (value) {
-                                    return controller.validateFormFields(value);
-                                  },
-                                  label: "Email",
-                                  bgColor: Get.theme.colorScheme.secondary,
-                                  textColor: Get.theme.colorScheme.tertiary,
-                                  icon: Icons.email,
-                                ),
-                                const SizedBox(height: 25),
-                                UiTextFormFieldWidget(
-                                  controller: controller.password,
-                                  validator: (value) {
-                                    return controller.validateFormFields(value);
-                                  },
-                                  label: "Password",
-                                  bgColor: Get.theme.colorScheme.secondary,
-                                  textColor: Get.theme.colorScheme.tertiary,
-                                  icon: Icons.password,
-                                  isPassword: true,
-                                ),
-                                const SizedBox(height: 25),
-                                UiButtonWidget(
-                                  text: controller.isLogin.value
-                                      ? 'Sign In'
-                                      : 'Sign Up',
-                                  width:
-                                      MediaQuery.of(context).size.width * .40,
-                                  height: 40,
-                                  onTap: () {
-                                    controller.signInOrUp();
-                                  },
-                                ),
-                                const SizedBox(height: 25),
-                                InkWell(
-                                  onTap: () {
-                                    controller.changeIsLogin();
-                                  },
-                                  child: RichText(
-                                    text: TextSpan(
-                                      children: [
-                                        TextSpan(
-                                            text:
-                                                '${controller.isLogin.value ? 'New to' : 'Already on'} "100 Days of Programming"? '),
-                                        TextSpan(
-                                          text: controller.isLogin.value
-                                              ? ' Sign Up'
-                                              : ' Sign In',
-                                          style: Get.theme.textTheme.bodyLarge!
-                                              .copyWith(
-                                            color:
-                                                Get.theme.colorScheme.secondary,
-                                            letterSpacing: 3,
-                                            fontWeight: FontWeight.bold,
+                                      ),
+                                      Obx(
+                                        () => controller.isLogin.value
+                                            ? const SizedBox()
+                                            : const SizedBox(height: 25),
+                                      ),
+                                      Obx(
+                                        () => controller.isLogin.value
+                                            ? const SizedBox()
+                                            : UiTextFormFieldWidget(
+                                                controller: controller.name,
+                                                validator: (value) {
+                                                  return controller
+                                                      .validateFormFields(
+                                                          value);
+                                                },
+                                                label: "Name",
+                                                bgColor: Get.theme.colorScheme
+                                                    .secondary,
+                                                textColor: Get
+                                                    .theme.colorScheme.tertiary,
+                                                icon: Icons.account_circle,
+                                              ),
+                                      ),
+                                      const SizedBox(height: 25),
+                                      UiTextFormFieldWidget(
+                                        controller: controller.email,
+                                        validator: (value) {
+                                          return controller
+                                              .validateFormFields(value);
+                                        },
+                                        label: "Email",
+                                        bgColor:
+                                            Get.theme.colorScheme.secondary,
+                                        textColor:
+                                            Get.theme.colorScheme.tertiary,
+                                        icon: Icons.email,
+                                      ),
+                                      const SizedBox(height: 25),
+                                      UiTextFormFieldWidget(
+                                        controller: controller.password,
+                                        validator: (value) {
+                                          return controller
+                                              .validateFormFields(value);
+                                        },
+                                        label: "Password",
+                                        bgColor:
+                                            Get.theme.colorScheme.secondary,
+                                        textColor:
+                                            Get.theme.colorScheme.tertiary,
+                                        icon: Icons.password,
+                                        isPassword: true,
+                                      ),
+                                      const SizedBox(height: 25),
+                                      UiButtonWidget(
+                                        text: controller.isLogin.value
+                                            ? 'Sign In'
+                                            : 'Sign Up',
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                .40,
+                                        height: 40,
+                                        onTap: () {
+                                          controller.signInOrUp();
+                                        },
+                                      ),
+                                      const SizedBox(height: 25),
+                                      InkWell(
+                                        onTap: () {
+                                          controller.changeIsLogin();
+                                        },
+                                        child: RichText(
+                                          text: TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                  text:
+                                                      '${controller.isLogin.value ? 'New to' : 'Already on'} "100 Days of Programming"? '),
+                                              TextSpan(
+                                                text: controller.isLogin.value
+                                                    ? ' Sign Up'
+                                                    : ' Sign In',
+                                                style: Get
+                                                    .theme.textTheme.bodyLarge!
+                                                    .copyWith(
+                                                  color: Get.theme.colorScheme
+                                                      .secondary,
+                                                  letterSpacing: 3,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                            style: Get
+                                                .theme.textTheme.labelLarge!
+                                                .copyWith(
+                                                    color: Get.theme.colorScheme
+                                                        .tertiary),
                                           ),
                                         ),
-                                      ],
-                                      style: Get.theme.textTheme.labelLarge!
-                                          .copyWith(
-                                              color: Get
-                                                  .theme.colorScheme.tertiary),
-                                    ),
+                                      )
+                                    ],
                                   ),
-                                )
-                              ],
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
                       ),
                     )),
