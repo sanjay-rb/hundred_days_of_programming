e431418f1be9b7b88f096729c3cbc1849be8b203
Sanjay Ramassery Babu <sanjaybabu618@gmail.com>
2024-07-28 16:59:13 +0530

    Patch : Fire function updated (#24)

--- functions/main.py
+++ functions/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from firebase_functions import scheduler_fn
 from firebase_admin import initialize_app, firestore
-import google.cloud.firestore
 from google.api_core.datetime_helpers import DatetimeWithNanoseconds
 
 # Application Default credentials are automatically created.
@@ -10,55 +9,43 @@ app = initialize_app()
 
 @scheduler_fn.on_schedule(schedule="0 0 * * *", timezone=scheduler_fn.Timezone("Asia/Kolkata"))
 def update_streak(event: scheduler_fn.ScheduledEvent) -> None:
-    print(event.job_name)
-    print(event.schedule_time)
-    db: google.cloud.firestore.Client = firestore.client()
-
+    db = firestore.client()
     users_ref = db.collection('users')
     users = users_ref.stream()
 
+    batch = db.batch()
+    current_date = datetime.now()
+
     for user in users:
-        # Getting referance of user document
         user_ref = user.reference
         user_data = user.to_dict()
-        name = user_data.get('name', 'Unknow User')
-        email = user_data.get('email', 'Unknow EmailID')
-        print("#"*50)
-        print(f"Checking User: {name} {email}")
-
-        # Reading current value
         last_submitted_date = user_data.get(
-            'lastSubmittedDate', DatetimeWithNanoseconds.now()
+            'lastSubmittedDate', DatetimeWithNanoseconds.now(),
         )
         streak = user_data.get('streak', 0)
         max_streak = user_data.get('maxStreak', 0)
-        current_date = datetime.now()
+
+        # Calculate day difference
         last_submitted_date = datetime.fromtimestamp(
             last_submitted_date.timestamp())
-
-        print(f"Current Streak Value {streak}")
-        print(f"Current Max Streak Value {max_streak}")
-
-        # Streak updation
         day_diff = (current_date - last_submitted_date).days
+
+        # Determine new streak value
         if day_diff == 1:
-            print("Adding value 1 to current streak.")
             new_streak = streak + 1
         elif day_diff > 1:
-            print("Resetting streak value to 0.")
             new_streak = 0
         else:
-            print("No change in streak.")
             new_streak = streak
 
-        # Max streak updation
+        # Update max streak if necessary
         if new_streak > max_streak:
             max_streak = new_streak
 
-        # Updation on firestore
-        user_ref.update({'streak': new_streak, 'maxStreak': max_streak})
-        print(f"Updated Streak Value {new_streak}")
-        print(f"Updated Max Streak Value {max_streak}")
-        print("#"*50)
+        # Prepare batch update
+        batch.update(user_ref, {'streak': new_streak, 'maxStreak': max_streak})
+
+    # Commit the batch update
+    batch.commit()
 
     print('Successfully updated streaks for all users')
